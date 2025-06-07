00ecb052014de742bf5f38056d0b1ac3a328835a
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-07 14:01:36 +0200

    AMI ID Lookup

--- data.tf
+++
@@ -1,4 +0,0 @@
-data "vault_kv_secret_v2" "example" {
-  mount = "secret"
-  name  = "AWS"
-}
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "ami_lookup" {
-  source      = "./modules/ami_lookup"
-  os_type     = "ubuntu"
-  architecture = "x86_64"  # or "arm64"
+  source       = "./modules/ami_lookup"
+  os_type      = var.os_type         # Can be set as workspace variable!
+  architecture = var.architecture    # Optional—uses default "x86_64" if unset
 }
 
 output "ami_id" {
