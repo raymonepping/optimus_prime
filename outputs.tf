a42d06ddc614692e6cad41737369e1a881f5d42d
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-07 13:13:06 +0200

    Output Setup

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "aws_region" {
   description = "The AWS region used for deployment"
   value       = var.region
 }
-
-output "aws_account_id" {
-  description = "AWS Account ID being used"
-  value       = data.aws_caller_identity.current.account_id
-}
