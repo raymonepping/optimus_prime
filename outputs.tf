73e6ef8b7d5a9eba1522efe605eff7e288d49632
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-07 15:24:56 +0200

    Add vpc module

--- outputs.tf
+++ outputs.tf
@@ -18,15 +18,47 @@ output "instance_public_ip" {
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
+}
+
+output "internet_gateway_id" {
+  value = module.vpc.internet_gateway_id
+}
+
+output "subnet_ids" {
+  value = module.vpc.subnet_ids
+}
+
 output "public_subnet_ids" {
   value = module.vpc.public_subnet_ids
 }
+
 output "private_subnet_ids" {
   value = module.vpc.private_subnet_ids
 }
-output "instance_id" {
-  value = module.compute.instance_id
+
+output "subnet_cidrs" {
+  value = module.vpc.subnet_cidrs
 }
-output "instance_public_ip" {
-  value = module.compute.public_ip
+
+output "public_route_table_id" {
+  value = module.vpc.public_route_table_id
+}
+
+output "private_route_table_ids" {
+  value = module.vpc.private_route_table_ids
+}
+
+output "availability_zones" {
+  value = module.vpc.availability_zones
+}
+
+output "vpc_main_route_table_id" {
+  value = module.vpc.vpc_main_route_table_id
+}
+
+output "vpc_default_security_group_id" {
+  value = module.vpc.vpc_default_security_group_id
 }
