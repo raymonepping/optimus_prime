e6534197e68311435b89c32139ae5b0d900a26a0
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-07 14:12:10 +0200

    AMI ID Lookup

--- modules/ami_lookup/main.tf
+++ modules/ami_lookup/main.tf
@@ -1,27 +1,36 @@
-data "aws_ami" "debug" {
+locals {
+  os_filters = {
+    ubuntu = {
+      name   = "ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"
+      owners = ["099720109477"]
+    }
+    redhat = {
+      name   = "RHEL-9*_HVM-*-x86_64-*"
+      owners = ["309956199498"]
+    }
+    suse = {
+      name   = "suse-sles-15-sp5-v202*-x86_64*"
+      owners = ["013907871322"]
+    }
+  }
+}
+
+data "aws_ami" "latest" {
   most_recent = true
-  owners      = ["099720109477"]
+  owners      = local.os_filters[var.os_type].owners
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/*24.04*"]
+    values = [local.os_filters[var.os_type].name]
   }
 
   filter {
     name   = "architecture"
-    values = ["x86_64"]
+    values = [var.architecture]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-}
-
-output "debug_ami_id" {
-  value = data.aws_ami.debug.id
-}
-
-output "debug_ami_name" {
-  value = data.aws_ami.debug.name
 }
