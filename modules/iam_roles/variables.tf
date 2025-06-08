06b444ef4db69838ae94377a6d5bf4812ef54869
Raymon Epping <raymon.epping@hashicorp.com>
2025-06-08 09:46:53 +0200

    Changed IAM roles

--- modules/iam_roles/variables.tf
+++ modules/iam_roles/variables.tf
@@ -1,3 +1,6 @@
 variable "project_name" { type = string }
 variable "environment"  { type = string }
-variable "tags"         { type = map(string); default = {} }
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
